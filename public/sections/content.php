41f1140bb48c9a297b2195630239424222bef8d3
Hatziry Vitales Herrera <hatziryvh@gmail.com>
2025-01-11 17:45:40 -0600

    añadiendo vista de contents

---
+++ public/sections/content.php
@@ -0,0 +1,31 @@
+<?php
+// Inicio del contenido del archivo
+?>
+
+<main>
+    <section>
+        <div class="accordion">
+            <div class="accordion-header" onclick="toggleAccordion(this)">
+                <h3>Unidad I - Contenido de la unidad</h3>
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7" />
+                </svg>
+            </div>
+            <div class="accordion-content">
+                <div class="link-container">
+                    <a href="#">Tema</a>
+                </div>
+                <div class="link-container">
+                    <a href="#">Tema</a>
+                </div>
+                <div class="link-container">
+                    <a href="#">Tema</a>
+                </div>
+            </div>
+        </div>
+    </section>
+</main>
+
+<?php
+// Fin del contenido del archivo
+?>
